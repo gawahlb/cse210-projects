7a60ca369204e7652b6640d34098c0abb0d7b8e5
gawahlb <wah23001@byui.edu>
2024-01-11 14:38:39 +0800

    added code for Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,100);
+
+        Console.WriteLine("I'm thinking of a Magic Number between 1 and 100.");
+
+        Console.WriteLine("What do you think the magic number is?");
+        string user_guess = Console.ReadLine();
+
+        int guess = int.Parse(user_guess);
+
+        while (guess != num)
+        {
+            if (guess > num)
+            {
+                Console.WriteLine("That's too high! Guess again: ");
+                string new_guess = Console.ReadLine();
+
+                guess = int.Parse(new_guess);
+            }
+            else
+            {
+                Console.WriteLine("That's too low! Guess again: ");
+                string new_guess = Console.ReadLine();
+
+                guess = int.Parse(new_guess);
+            }
+        }
+
+        Console.WriteLine("You guessed correctly! Great job!");
+    
     }
 }
