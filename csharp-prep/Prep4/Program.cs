3e09474a476b4d9d278d29c4d0976a0c6a1a5fe8
gawahlb <wah23001@byui.edu>
2024-01-11 15:17:41 +0800

    added code for Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int num = 1;
+
+        while (num != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            string user_num = Console.ReadLine();
+
+            num = int.Parse(user_num);
+
+            numbers.Add(num);
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        int num_count = numbers.Count;
+
+        float avg = ((float)sum) / num_count;
+
+        Console.WriteLine($"The average is: {avg}");
+
+        int new_num = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > new_num)
+            {
+                new_num = number;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is: {new_num}");
     }
 }
