88c88a99ec10ad05e3203e30cd93fdf45542703c
gawahlb <wah23001@byui.edu>
2024-01-11 13:47:58 +0800

    Completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
