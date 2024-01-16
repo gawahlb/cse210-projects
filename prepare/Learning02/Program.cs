a2ee94d3e18a0aa85338c9799220c60bb70cb7c4
gawahlb <wah23001@byui.edu>
2024-01-16 20:50:24 +0800

    made learning2 programs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job2._jobTitle = "Manager";
+        job1._company = "Microsoft";
+        job2._company = "Apple";
+        job1._startYear = 2019;
+        job2._startYear = 2022;
+        job1._endYear = 2022;
+        job2._endYear = 2023;
+
+        Resume mine = new Resume();
+
+        mine._jobs.Add(job1);
+        mine._jobs.Add(job2);
+        mine._name = "Allison Rose";
+    
+        mine.Display();
     }
 }
