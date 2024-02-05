f470fbab14c393a58311bceade48da0f396ab2f8
gawahlb <wah23001@byui.edu>
2024-02-05 10:10:20 -0700

    finished learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+
+        Square a = new("Red", 3);
+        Rectangle b = new("Blue",4,5);
+        Circle c = new("Green", 6);
+
+        shapes.Add(a);
+        shapes.Add(b);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            System.Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
